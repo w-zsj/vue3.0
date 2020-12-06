d1df5fde667dbef828be2660ce5d6e49cd962e2b
zhangsanjun <zhangsanjun@eyee.com>
2020-12-06 21:06:27 +0800

    U- add

--- vue.config.js
+++ vue.config.js
@@ -37,13 +37,13 @@ module.exports = {
         // 启用 CSS modules for all css / pre-processor files.
         requireModuleExtension: false
     },
-    devServer: {// 环境配置
+    devServer: {
         host: '0.0.0.0',
-        port: 15011,
+        port: 15302,
         https: false,
         hotOnly: false,
-        open: true, //配置自动启动浏览器
-
+        // proxy: {}, // 设置代理
+        // before: app => { }
     },
     pluginOptions: {// 第三方插件配置
         // ...
