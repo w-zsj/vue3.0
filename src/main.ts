53839629b5027103d6c46d32d8584bd3e4cdec49
zhangsanjun <zhangsanjun@eyee.com>
2022-04-20 07:51:19 +0800

    ssr

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
-import { createApp } from 'vue'
 import App from './App.vue'
-import router from "./router"
-import store from './store/index'
-import setupVant from './vantComps'
-// import 'vant/lib/index.css';
-const app = createApp(App, { title: "app" })
-app.use(router)
-app.use(store)
-setupVant(app)
+import { createSSRApp } from 'vue'
+import { createRouter } from './router'
 
-app.config.globalProperties.$http = 'test';
-
-router.isReady().then(() => {
-    app.mount("#app");
-});
+// SSR requires a fresh app instance per request, therefore we export a function
+// that creates a fresh app instance. If using Vuex, we'd also be creating a
+// fresh store here.
+export function createApp() {
+  const app = createSSRApp(App)
+  const router = createRouter()
+  app.use(router)
+  return { app, router }
+}
---
+++ src/pages/home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <van-list finished-text="没有更多了">
+      <van-cell>12</van-cell>
+      <van-cell>12</van-cell>
+      <van-cell>12</van-cell>
+    </van-list>
+    <van-checkbox-group v-model="checked" direction="horizontal">
+      <van-checkbox name="a">复选框 a</van-checkbox>
+      <van-checkbox name="b">复选框 b</van-checkbox>
+    </van-checkbox-group>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Index",
+  setup(props) {
+    console.log("props--->>>", props);
+    return {};
+  },
+});
+</script>
+<style scoped lang="scss">
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+  background-color: #39a9ed;
+}
+</style>
