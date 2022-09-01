418df20653bf7d5894dbf87c57b2f14d92c253bc
zhangsanjun <zhangsanjun@eyee.com>
2022-09-01 11:54:48 +0800

    dd

--- src/Layout/BasicLayout.vue
+++ src/Layout/BasicLayout.vue
@@ -1,11 +1,13 @@
 <template>
   <el-container>
     <el-aside>
-      <el-scrollbar height="100vh">
+      <div class="admin-name flex-center">电商管理后台</div>
+      <el-scrollbar>
         <SiderMenu />
       </el-scrollbar>
     </el-aside>
     <el-container>
+      <div class="user-info flex-end">admin</div>
       <el-header>
         <Breadcrumb />
       </el-header>
@@ -28,7 +30,18 @@ export default defineComponent({
   setup() {},
 });
 </script>
-<style scoped lang='scss'>
+<style scoped lang="scss">
+.admin-name {
+  height: 80px;
+  color: #fff;
+  position: sticky;
+  top: 0;
+  z-index: 9;
+}
+.user-info {
+  height: 80px;
+  padding-right: 40px;
+}
 .el-header {
   background-color: #f9f9f9;
   color: #333;
@@ -39,16 +52,19 @@ export default defineComponent({
 .el-aside {
   background-color: #1f2d3d;
   max-width: 200px;
+  .el-scrollbar {
+    height: calc(100vh - 80px);
+  }
 }
+
 .el-main {
   background-color: #e9eef3;
   color: #333;
   text-align: center;
-  height: calc(100vh - 60px);
+  height: calc(100vh - 60px - 80px);
+  // overflow: hidden;
 }
-
 body > .el-container {
   margin-bottom: 40px;
 }
 </style>
-
